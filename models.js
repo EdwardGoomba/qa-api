40bc066c39f1e95b0aca288b378d293a5c690083
EdwardGoomba <ask@edwardvdesign.com>
2018-02-10 10:18:44 -0500

    Built out q and a schemas. Added update, vote, sort methods.

--- models.js
+++ models.js
@@ -4,6 +4,16 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const sortAnswers = function(a, b) {
+  // - negative a before b
+  // 0 no change
+  // + positive a after b
+  if (a.votes === b.votes) {
+    return b.updatedAt - a.updatedAt;
+  }
+  return b.votes - a.votes;
+}
+
 const AnswerSchema = new Schema({
   text: String,
   createdAt: {type: Date, default: Date.now},
@@ -11,10 +21,30 @@ const AnswerSchema = new Schema({
   votes: {type: Number, default: 0}
 });
 
+AnswerSchema.method('update', function(updates, callback) {
+  Object.assign(this, updates, {updatedAt: new Date()});
+  // save parent document
+  this.parent().save(callback);
+});
+
+AnswerSchema.method('vote', function(vote, callback) {
+  if (vote === 'up') {
+    this.votes += 1;
+  } else {
+    this.votes -= 1;
+  }
+  this.parent().save(callback);
+});
+
 const QuestionSchema = new Schema({
   text: String,
   createdAt: {type: Date, default: Date.now},
   answers: [AnswerSchema]
+});
+
+QuestionSchema.pre('save', function(next) {
+  this.answers.sort(sortAnswers);
+  next();
 });
 
 const Question = mongoose.model('Question', QuestionSchema);
