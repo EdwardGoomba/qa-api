5b053a9f11943ba73e3477f48493ed15886f1e59
EdwardGoomba <ask@edwardvdesign.com>
2018-02-10 09:02:23 -0500

    Setup schema

--- app.js
+++ app.js
@@ -10,6 +10,22 @@ const logger = require('morgan');
 app.use(logger('dev'));
 app.use(jsonParser());
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/qa');
+
+const db = mongoose.connection;
+
+db.on('error', function(err) {
+  console.error('connection error:', err);
+});
+
+db.once('open', function() {
+  console.log('db connection successful');
+  // All database comms here
+  
+});
+
 app.use('/questions', routes);
 
 // catch 404 and forward to error handler
