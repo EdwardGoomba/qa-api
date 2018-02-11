ce43caec3b3a41e3b7481a1368507196f4dd3855
EdwardGoomba <edward.danilyuk@gmail.com>
2018-02-10 23:20:56 -0500

    Debug

--- models.js
+++ models.js
@@ -5,46 +5,45 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const sortAnswers = function(a, b) {
-  // - negative a before b
-  // 0 no change
-  // + positive a after b
-  if (a.votes === b.votes) {
-    return b.updatedAt - a.updatedAt;
-  }
-  return b.votes - a.votes;
+	//- negative a before b
+	//0 no change
+	//+ positive a after b
+	if(a.votes === b.votes){
+		return b.updatedAt - a.updatedAt;
+	}
+	return b.votes - a.votes;
 }
 
 const AnswerSchema = new Schema({
-  text: String,
-  createdAt: {type: Date, default: Date.now},
-  updatedAt: {type: Date, default: Date.now},
-  votes: {type: Number, default: 0}
+	text: String,
+	createdAt: {type: Date, default: Date.now},
+	updatedAt: {type: Date, default: Date.now},
+	votes: {type: Number, default:0}
 });
 
 AnswerSchema.method('update', function(updates, callback) {
-  Object.assign(this, updates, {updatedAt: new Date()});
-  // save parent document
-  this.parent().save(callback);
+	Object.assign(this, updates, {updatedAt: new Date()});
+	this.parent().save(callback);
 });
 
 AnswerSchema.method('vote', function(vote, callback) {
-  if (vote === 'up') {
-    this.votes += 1;
-  } else {
-    this.votes -= 1;
-  }
-  this.parent().save(callback);
+	if(vote === 'up') {
+		this.votes += 1;
+	} else {
+		this.votes -= 1;
+	}
+	this.parent().save(callback);
 });
 
 const QuestionSchema = new Schema({
-  text: String,
-  createdAt: {type: Date, default: Date.now},
-  answers: [AnswerSchema]
+	text: String,
+	createdAt: {type: Date, default: Date.now},
+	answers: [AnswerSchema]
 });
 
-QuestionSchema.pre('save', function(next) {
-  this.answers.sort(sortAnswers);
-  next();
+QuestionSchema.pre('save', function(next){
+	this.answers.sort(sortAnswers);
+	next();
 });
 
 const Question = mongoose.model('Question', QuestionSchema);
